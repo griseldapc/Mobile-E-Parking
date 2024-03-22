43846b969686e28c2d01ef3416c52978da9a01a6
griseldapc <boosycen@gmail.com>
2024-03-22 15:39:32 +0700

    update

--- src/pages/manual.js
+++ src/pages/manual.js
@@ -13,7 +13,7 @@ export default function Manual() {
                     </Link>
                     <h1 style={{ fontSize: "16px", fontWeight: "600", textAlign: "center", marginBottom: 0 }}>Tambahkan Tiket Manual</h1>
                 </div>
-                <div className="home my-auto" style={{ display: "flex", flexDirection: "column", justifyContent: "center", gap: "30px", alignItems: "center", height: "77vh" }}>
+                <div className="home my-auto" style={{ display: "flex", flexDirection: "column", justifyContent: "center", gap: "30px", alignItems: "center", height: "100vh" }}>
                     <Link to="/manualMasuk" className="mx-auto">
                         <img src="../../assets/Ticket-masuk.svg" className="mg mx-auto" style={{ width: "100%" }}></img>
                     </Link>
