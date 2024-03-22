35a7ee5ba9ac97574d8508b2bcd890af0b3822b6
griseldapc <boosycen@gmail.com>
2024-03-22 14:43:27 +0700

    update

--- src/pages/sukses.js
+++ src/pages/sukses.js
@@ -4,14 +4,14 @@ import Header from "../components/header";
 
 export default function SuksesKeluar() {
     return (
-        <section className="mx-auto" style={{ backgroundColor: "#269544", maxHeight: "100vh", maxWidth: "376px" }}>
+        <section className="mx-auto" style={{ backgroundColor: "#269544", maxHeight: "105vh", maxWidth: "376px" }}>
             <Header />
             <div>
                 <div className="home" style={{ boxShadow: "0px 2px 4px rgba(201, 201, 201, 0.2)" }}>
                     <h1 style={{ fontSize: "16px", fontWeight: "600", width: "100%", textAlign: "center", color: "white" }}>Scan Tiket</h1>
                 </div>
 
-                <div className="home" style={{ display: "flex", flexDirection: "column", justifyContent: "end", height: "75vh", gap: "30%" }}>
+                <div className="home" style={{ display: "flex", flexDirection: "column", justifyContent: "end", height: "78vh", gap: "30%" }}>
                     <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
                         <p style={{ fontSize: "16px", fontWeight: "bold", color: "white" }}>Data tiket telah berhasil disimpan
                         </p>
