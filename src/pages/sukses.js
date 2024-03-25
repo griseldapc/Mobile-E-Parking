7e30ae68a695661a809ef33230075a19c1103851
griseldapc <boosycen@gmail.com>
2024-03-25 08:47:21 +0700

    update

--- src/pages/sukses.js
+++ src/pages/sukses.js
@@ -6,12 +6,12 @@ export default function Sukses() {
     return (
         <section className="mx-auto" style={{ backgroundColor: "#269544", maxWidth: "376px" }}>
             <Header />
-            <div className="home">
-                <div style={{ boxShadow: "0px 2px 4px rgba(201, 201, 201, 0.2)" }}>
+            <div>
+                <div style={{ boxShadow: "0px 2px 4px rgba(201, 201, 201, 0.2)", paddingBottom: "10px" }}>
                     <h1 style={{ fontSize: "16px", fontWeight: "600", width: "100%", textAlign: "center", color: "white" }}>Scan Tiket</h1>
                 </div>
-                <div>
-                    <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", height: "100vh", textAlign: "center", paddingBottom: "10px" }}>
+                <div className="home">
+                    <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", height: "80vh", textAlign: "center", bottom: "20px" }}>
                         <img src="../../assets/sukses.svg" className="mx-auto" alt="Success"></img>
                         <p style={{ fontSize: "16px", fontWeight: "bold", color: "white" }}>Data tiket telah berhasil disimpan
                         </p>
